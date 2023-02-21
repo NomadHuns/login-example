57208816998df87da87653ced06669a76906026b
kimalss22 <kimalss22@gmail.com>
2023-02-21 09:43:04 +0900

    프로젝트 생성

---
+++ src/test/java/shop/mtcoding/loginexam/LoginexamApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.loginexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
