053ee29f9ef17df0d2b4d2784abc17679c045827
kimalss22 <kimalss22@gmail.com>
2023-02-21 15:32:31 +0900

    user 모델링 salt 필드 추가
    user_tb salt 칼럼 추가
    user 모델 객체 salt 필드 추가
    더미 데이터 salt 값 추가

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,4 +1,4 @@
-insert into user_tb(username, password, email, created_at) values('ssar', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'ssar@nate.com', now());
-insert into user_tb(username, password, email, created_at) values('cos', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'cos@nate.com', now());
+insert into user_tb(username, password, salt, email, created_at) values('ssar', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'SmGXj3iJfUY3TjqFms+5/g==', 'ssar@nate.com', now());
+insert into user_tb(username, password, salt, email, created_at) values('cos', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4','SmGXj3iJfUY3TjqFms+5/g==', 'cos@nate.com', now());
 
 commit; 
